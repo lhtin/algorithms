a0f8b88dc90d96793b34bb885af61742f65f6f39
01nomagic <784694610@qq.com>
2020-01-13 00:21:56 +0800

    优化注释和readme

--- SortingAlgorithms.cpp
+++ SortingAlgorithms.cpp
@@ -7,6 +7,7 @@
 #include <utility>
 using namespace std;
 
+/***** 工具函数，协助打印信息和调用排序算法 *****/
 void printArr (const int arr[], int len) {
   cout << "[";
   for (int i = 0; i < len; i += 1) {
@@ -29,9 +30,11 @@ void run (Sort sort) {
   cout << endl;
 }
 
-// https://en.wikipedia.org/wiki/Insertion_sort
-// 插入排序，稳定排序，时间复杂度：最坏n*(n-1)，最好n，不过交换的次数过多
-// 算法描述：依次将待排序列表的第一个元素归入到已排序列表的最后一个元素，然后从已排序列表的最后一个元素开始，比较其跟前一个元素的大小，小于则交换位置，然后继续跟跟前面的元素比较，直到到达了最前面或者大于等于前面的元素。
+/**
+ * 插入排序
+ * 稳定排序，时间复杂度：最坏n*(n-1)，最好n，不过交换的次数过多
+ * 算法描述：依次将待排序列表的第一个元素归入到已排序列表的最后一个元素，然后从已排序列表的最后一个元素开始，比较其跟前一个元素的大小，小于则交换位置，然后继续跟跟前面的元素比较，直到到达了最前面或者大于等于前面的元素。
+ */
 void InsertionSort (int arr[], int len) {
   printSortName(__func__);
   for (int i = 0; i < len; i += 1) {
@@ -45,9 +48,11 @@ void InsertionSort (int arr[], int len) {
   }
 }
 
-// https://en.wikipedia.org/wiki/Selection_sort
-// 选择排序，稳定排序，时间复杂度：最坏和最好都是n*(n-1)，交换的次数最少，但是比较的次数最多
-// 算法描述：依次从待排序列表中找出最小值，然后跟待排序列表的第一个元素交换，交换完之后将第一个元素归入已排序列表，直到待排序列表为空。
+/**
+ * 选择排序
+ * 稳定排序，时间复杂度：最坏和最好都是n*(n-1)，交换的次数最少，但是比较的次数最多
+ * 算法描述：依次从待排序列表中找出最小值，然后跟待排序列表的第一个元素交换，交换完之后将第一个元素归入已排序列表，直到待排序列表为空。
+ */
 void SelectionSort (int arr[], int len) {
   printSortName(__func__);
   for (int i = 0; i < len; i += 1) {
@@ -68,11 +73,13 @@ void SelectionSort (int arr[], int len) {
   }
 }
 
-// https://en.wikipedia.org/wiki/Merge_sort
-// 归并排序，稳定排序，时间复杂度稳定在nlogn，空间复杂度n
-// 算法描述：
-//   1. 将两个已排序列表合并成一个。从两个列表的第一个元素开始比较，如果第一个列表的元素小于等于第二个列表的元素，则将第一个列表的元素添加到结果列表的后面，否则将第二个列表的元素添加到结果列表的后面。直到一个列表的所有元素都添加到了结果列表后，将另一个列表的剩余元素依次添加到结果列表的后面
-//   2. 递归的将待排序列表分割成两个待排序列表，然后分别对这两个待排序列表递归排序。如果待排序列表只有一个元素，则认为已经排好序。
+/**
+ * 归并排序
+ * 稳定排序，时间复杂度稳定在nlogn，空间复杂度n
+ * 算法描述：
+ *   1. 将两个已排序列表合并成一个。从两个列表的第一个元素开始比较，如果第一个列表的元素小于等于第二个列表的元素，则将第一个列表的元素添加到结果列表的后面，否则将第二个列表的元素添加到结果列表的后面。直到一个列表的所有元素都添加到了结果列表后，将另一个列表的剩余元素依次添加到结果列表的后面
+ *   2. 递归的将待排序列表分割成两个待排序列表，然后分别对这两个待排序列表递归排序。如果待排序列表只有一个元素，则认为已经排好序。
+ */
 void _MergeSort (int arr[], int temp[], int start, int end) {
   if (start == end) {
     // 只有一个元素时直接返回
@@ -127,12 +134,13 @@ void MergeSort (int arr[], int len) {
   _MergeSort(arr, temp, 0, len - 1);
 }
 
-// https://en.wikipedia.org/wiki/Heapsort
-// https://en.wikipedia.org/wiki/Heap_(data_structure)
-// 堆数据结构：最大二叉堆(max binary heap)指父节点大于子节点的二叉树结构
-// 堆排序算法：
-//   1. 构造一个最大堆
-//   2. 将根节点与最后的子节点交换然后移除最后的子节点，并让剩余堆维持成一个最大堆，直到剩余堆为空，则移除的所有子节点从小到达排好了顺序
+/**
+ * 堆排序
+ * 堆数据结构：最大二叉堆(max binary heap)指父节点大于子节点的二叉树结构
+ * 堆排序算法：
+ *   1. 构造一个最大堆
+ *   2. 将根节点与最后的子节点交换然后移除最后的子节点，并让剩余堆维持成一个最大堆，直到剩余堆为空，则移除的所有子节点从小到达排好了顺序
+ */
 void _ToMaxHeap (int *arr, int len) {
   for (int i = 0; i < len; i += 1) {
     int child = i;
@@ -157,8 +165,10 @@ void HeapSort (int arr[], int len) {
   }
 }
 
-// https://en.wikipedia.org/wiki/Quicksort
-// 快速排序，不稳定，平均nlogn，最坏n*n
+/**
+ * 快速排序
+ * 不稳定，平均nlogn，最坏n*n
+ */
 int partition (int arr[], int start, int end) {
   int i = start, j = end;
   int mid = (int)(start + (end - start) / 2);
@@ -190,8 +200,10 @@ void QuickSort (int arr[], int len) {
   _QuickSort(arr, 0, len - 1);
 }
 
-// https://en.wikipedia.org/wiki/Shellsort
-// 希尔排序：插入排序的改进版，不稳定排序，平均nlogn，最坏n*n
+/**
+ * 希尔排序
+ * 插入排序的改进版，不稳定排序，平均nlogn，最坏n*n
+ */
 void _InsertionSortWithGap (int arr[], int len, int gap, int start) {
   // InsertionSort的变体，增加gap和start参数，用于指定具体给哪些元素排序
   for (int i = start; i < len; i += gap) {
@@ -214,7 +226,6 @@ void ShellSort (int arr[], int len) {
 }
 
 int main () {
-
   run(InsertionSort);
   run(SelectionSort);
   run(MergeSort);
@@ -224,7 +235,3 @@ int main () {
 
   return 0;
 }
-
-
-
-
