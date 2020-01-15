4fbf5ddd7965b6e35b27ffec3f7f69cf46870ba0
01nomagic <784694610@qq.com>
2020-01-15 23:15:33 +0800

    add more leetcode anwsers

---
+++ leetcode/20-valid-parentheses.cpp
@@ -0,0 +1,44 @@
+#include "leetcode.h"s
+
+class Solution {
+public:
+  bool isValid(string s) {
+    string temp;
+    for (int i = 0, len = s.size(); i < len; i += 1) {
+      char c = s[i];
+      if (c == '(' || c == '{' || c == '[') {
+        temp.push_back(c);
+      } else {
+        int last = temp.size() - 1;
+        if (last < 0) {
+          return false;
+        }
+        char last_c = temp[last];
+        if (c == ')') {
+          if (last_c == '(') {
+            temp.pop_back();
+          } else {
+            return false;
+          }
+        } else if (c == '}') {
+          if (last_c == '{') {
+            temp.pop_back();
+          } else {
+            return false;
+          }
+        } else if (c == ']') {
+          if (last_c == '[') {
+            temp.pop_back();
+          } else {
+            return false;
+          }
+        }
+      }
+    }
+    if (temp.size() == 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+};
